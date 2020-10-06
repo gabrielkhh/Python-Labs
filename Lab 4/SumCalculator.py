7582cfcfe3afe67a558c6ced9e16a6770209d30d
Gabriel Kok <gabrielkok4227@gmail.com>
2020-10-06 21:02:24 +0800

    Added Lab 4 This update adds the Lab 4 folder and SumCalculator.py

---
+++ Lab 4/SumCalculator.py
@@ -0,0 +1,37 @@
+import sys
+
+try:
+    # Get the input from the parameters
+    input_number = int(sys.argv[1])
+except ValueError:
+    print("Your input is invalid!")
+    sys.exit()
+except IndexError:
+    print("Your input is invalid!")
+    sys.exit()
+
+def sum_recursive(x):
+    if x == 1:
+        return 1
+    elif x > 1:
+        return sum_recursive(x - 1) + x
+
+def sum_iterative(x):
+    sum = 0
+    while x > 0:
+        sum += x
+        x -= 1
+
+    return sum
+
+result_recursive = 0
+result_iterative = 0
+
+if input_number > 0:
+    result_recursive = sum_recursive(input_number)
+    result_iterative = sum_iterative(input_number)
+else:
+    print("Your input is invalid!")
+    sys.exit()
+
+print("The SUM value calculated by recursive is %d and by iterative is %d." % (result_recursive, result_iterative))
