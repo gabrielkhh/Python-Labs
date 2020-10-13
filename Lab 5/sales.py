e56bc9977b4bcaba69144c7883171585f2686f86
Gabriel Kok <gabrielkok4227@gmail.com>
2020-10-14 00:45:10 +0800

    Additional Cleanups

--- Lab 5/sales.py
+++ Lab 5/sales.py
@@ -2,11 +2,9 @@ import sys
 from functools import reduce
 
 try:
-    # Get the input from the parameters
     input_numbers = str(sys.argv[1])
     sales_list = [int(s) for s in input_numbers.split(',')]
     scale_factor = int(sys.argv[2])
-
 except ValueError:
     print("Your input is invalid!")
     sys.exit()
@@ -24,7 +22,9 @@ def sort(list1):
 
 
 def goodSales(list1):
-    total_sum = reduce(lambda a,b: a + b, list1)
-    return list(filter(lambda c: ((c > (total_sum/len(list1))) is True), list1))
+    total_sum = reduce(lambda a, b: a + b, list1)
+    return list(filter(lambda c: ((c > (total_sum / len(list1))) is True), list1))
 
-print("The scaled number is: %a The sorted sales numbers are: %a The good sales numbers are: %a" % (scale(sales_list, scale_factor), sorted(sales_list), goodSales(sales_list)))
+
+print("The scaled number is: %a The sorted sales numbers are: %a The good sales numbers are: %a" % (
+    scale(sales_list, scale_factor), sorted(sales_list), goodSales(sales_list)))
